3a79426f3d2f150d3ffb25e82a15e42214002627
slmpersonal <103977696+slmpersonal@users.noreply.github.com>
2022-10-28 00:22:42 -0500

    Update md5.py
    Simplified and more stable code.

--- md5.py
+++ md5.py
@@ -107,7 +107,19 @@ def md5(msg):
 
 
 def md5_to_hex(dig):
-    raw = dig.to_bytes(16, byteorder='little')
+    # try:  ##  Optional alternative solution to Overflow errors
+        # raw = dig.to_bytes(16, byteorder='little')
+    # except:
+        # dig = dig - 340282366920938463463374607431768211456
+        # raw = dig.to_bytes(16, byteorder='little')
+    # return '{:032x}'.format(int.from_bytes(raw, byteorder='big'))
+    
+    try: ##  Remove extra Hex characters causing Overflow error
+        raw = dig.to_bytes(16, byteorder='little')
+    except:
+        dig = "0" + str(hex(dig))[2:]
+        dig = int(dig[2:],16)
+        raw = dig.to_bytes(16, byteorder='little')
     return '{:032x}'.format(int.from_bytes(raw, byteorder='big'))
 
 
